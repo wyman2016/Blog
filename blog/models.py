e1ca04d98f86c8bae9c2abadd39b086a980a47d1
wuyan <wuyan@funtsui.com>
2019-01-24 17:00:11 +0700

    no message

--- blog/models.py
+++ blog/models.py
@@ -11,7 +11,7 @@ class Blog(models.Model):
     blog_type = models.ForeignKey(BlogType, on_delete=models.DO_NOTHING)
     content = RichTextUploadingField()
     author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    create_time = models.DateTimeField(auto_now_add=True)
+    create_time = models. (auto_now_add=True)
     last_updated_time = models.DateTimeField(auto_now=True)
     def __str__(self):
         return "<Blog: %s>" % self.title
